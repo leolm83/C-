8837ce60058ac401a7934c74c51151bb190320e5
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-04-18 14:45:51 -0300

    outra maneira para fibonacci
    Adicionando outra maneira de executar a sequencia, exatamente como está na documentacao

--- numerosfibonacci.cs
+++ numerosfibonacci.cs
@@ -8,3 +8,14 @@ for(int x=2;x<20;x++){
     fibonacciNumbers.Add(fibonacciNumbers[x-1]+fibonacciNumbers[x-2]);
     Console.WriteLine($"{fibonacciNumbers[fibonacciNumbers.Count-1]}");
 }
+/*var fibonacciNumbers = new List<int> {1, 1};
+
+while (fibonacciNumbers.Count < 20)
+{
+    var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+    var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+    fibonacciNumbers.Add(previous + previous2);
+}
+foreach(var item in fibonacciNumbers)
+    Console.WriteLine(item);*/
