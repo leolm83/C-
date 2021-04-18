2db3c078b2cf67465362c23e94643550c5f68712
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-04-18 14:40:46 -0300

    Create numerosfibonacci.cs
    Adicionando um uso da sequencia fibonacci ao repositório

---
+++ numerosfibonacci.cs
@@ -0,0 +1,10 @@
+var fibonacciNumbers = new List<int> {1, 1};
+/*var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+fibonacciNumbers.Add(previous + previous2);*/
+
+for(int x=2;x<20;x++){
+    fibonacciNumbers.Add(fibonacciNumbers[x-1]+fibonacciNumbers[x-2]);
+    Console.WriteLine($"{fibonacciNumbers[fibonacciNumbers.Count-1]}");
+}
